95f0426a2d783911028714f144f81fac979b7233
Arshan <arshandalili@gmail.com>
2022-07-23 21:32:10 +0430

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -2,8 +2,10 @@
 
 (require "interpreter.rkt")
 
+(define (evaluate-string program)
+  (run program))
 
-(define (evaluate path) (run (file->string path)))
-
+(define (evaluate path)
+  (run (file->string path)))
 
 (evaluate "./tests/binary-search.txt")
